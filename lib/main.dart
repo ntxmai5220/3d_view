cb9f72417720db939157d5e3728bc927b08a370b
Thanh Nam <dtnam302@gmail.com>
2021-12-28 14:40:26 +0700

    test 2

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test branch Nam',
+      title: 'Test branch Nam 2',
       theme: ThemeData(backgroundColor: Colors.white),
       home: const LoginPage(),
     );

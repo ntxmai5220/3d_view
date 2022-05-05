30a5012c83c4cb327bf931ec9488b4d705971a9b
minhpro <congminhpru@gmail.com>
2022-05-05 23:43:56 +0700

    Implement auth api

---
+++ application/lib/minhApis/auth/authRequest.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+
+import 'package:bk_3d_view/minhApis/apis.dart';
+import 'package:dio/dio.dart';
+
+class AuthRequest {
+    AuthRequest._();
+
+
+    static DioRequest login({required Map<String, String> account}){
+      Options options = OptionBuilder().jsonContent().build();
+      return DioRequest(path: "auth/login", body: jsonEncode(account), options: options );
+    }
+
+    static DioRequest signUp({required Map<String, String> account}){
+      Options options = OptionBuilder().jsonContent().build();
+      return DioRequest(path: "auth/register", body: jsonEncode(account), options: options );
+    }
+}

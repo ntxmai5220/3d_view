3adf87eb9b34878fee9c1ad2558a4550411c338d
minhpro <congminhpru@gmail.com>
2022-05-05 22:12:58 +0700

    implement auth

--- application/lib/minhApis/base/DioClient.dart
+++ application/lib/minhApis/base/DioClient.dart
@@ -5,15 +5,19 @@ import 'package:dio/dio.dart';
 class DioClient {
   static final Dio _dio = Dio();
   static final baseUrl = "https://thesis-api.onrender.com/v1/3dview/";
-  
-  static String buildFullPath(String path){
-    return baseUrl+path;
+
+  static String buildFullPath(String path) {
+    return baseUrl + path;
   }
 
-  static Future<dynamic> get({required String path, Map<String,dynamic>? params, Options? options}) async {
+  static Future<dynamic> get(
+      {required String path,
+      Map<String, dynamic>? params,
+      Options? options}) async {
     String fullPath = buildFullPath(path);
     try {
-      Response response = await _dio.get(fullPath,queryParameters: params, options: options);
+      Response response =
+          await _dio.get(fullPath, queryParameters: params, options: options);
       return response.data;
     } catch (e) {
       print(e.toString());
@@ -21,10 +25,12 @@ class DioClient {
     return null;
   }
 
-  static Future<dynamic> post({required String path, dynamic data, Options? options}) async {
+  static Future<dynamic> post(
+      {required String path, dynamic data, Options? options}) async {
     String fullPath = buildFullPath(path);
     try {
-      Response response = await _dio.post(fullPath,data:data, options: options );
+      Response response =
+          await _dio.post(fullPath, data: data, options: options);
       return response.data;
     } catch (e) {
       print(e.toString());
@@ -32,14 +38,17 @@ class DioClient {
     return null;
   }
 
-  static Future<dynamic> put({required String path, dynamic data,  Options? options}) async {
+  static Future<dynamic> put(
+      {required String path, dynamic data, Options? options}) async {
     String fullPath = buildFullPath(path);
     try {
-      Response response = await _dio.put(fullPath,data:data, options: options);
+      Response response =
+          await _dio.put(fullPath, data: data, options: options);
       return response.data;
     } catch (e) {
       print(e.toString());
     }
     return null;
   }
+
 }
--- application/test/api_test.dart
+++ application/test/api_test.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:bk_3d_view/minhApis/auth/authServices.dart';
 import 'package:bk_3d_view/minhApis/inpanting/inpantingServices.dart';
 import 'package:bk_3d_view/minhApis/post/postServices.dart';
 import 'package:bk_3d_view/models/models.dart';
@@ -10,8 +11,8 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   String token = "example1";
 
-  Future< List<MapEntry<String,Uint8List>>> getImage() async {
-    List<MapEntry<String,Uint8List>> images = [];
+  Future<List<MapEntry<String, Uint8List>>> getImage() async {
+    List<MapEntry<String, Uint8List>> images = [];
 
     File image = File("assets/test/anh1.jpg");
     String name = image.path.split("/").last;
@@ -43,8 +44,13 @@ void main() {
       'ward[name]': 'Phuong 9',
       'ward[districtId]': '80'
     };
-    List<String> imageDescription = ["test1","test2"];
-    await services.createPost(id: "625bd0648e18145a85211945", images: images, imageDescription: imageDescription ,landInfo: landInfo, token: token);
+    List<String> imageDescription = ["test1", "test2"];
+    await services.createPost(
+        id: "625bd0648e18145a85211945",
+        images: images,
+        imageDescription: imageDescription,
+        landInfo: landInfo,
+        token: token);
   });
 
   test("add hotpost", () async {
@@ -67,54 +73,61 @@ void main() {
       }
     ];
     PostServices services = PostServices();
-    await services.addHotspot(id:"625be882ba0392826c179527", hotspots: hotspots, token: token );
+    await services.addHotspot(
+        id: "625be882ba0392826c179527", hotspots: hotspots, token: token);
   });
 
   test("upload thubmnail", () async {
     final images = await getImage();
     PostServices services = PostServices();
-    await services.uploadThumbnail(id:"625be882ba0392826c179527", images: images , token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNWJkMDY0OGUxODE0NWE4NTIxMTk0NSIsImlzQWRtaW4iOmZhbHNlLCJzZXNzaW9uVG9rZW4iOiI2MjViZTU0MWQ3Y2UxY2JjMGJiZTZjZjUiLCJpYXQiOjE2NTAxODk2MzMsImV4cCI6MTY1Mjc4MTYzM30.NpfugyhGnauxPcW9rdHgj4oEf_GRS84HznepD3X14Sw" );
+    await services.uploadThumbnail(
+        id: "625be882ba0392826c179527",
+        images: images,
+        token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNWJkMDY0OGUxODE0NWE4NTIxMTk0NSIsImlzQWRtaW4iOmZhbHNlLCJzZXNzaW9uVG9rZW4iOiI2MjViZTU0MWQ3Y2UxY2JjMGJiZTZjZjUiLCJpYXQiOjE2NTAxODk2MzMsImV4cCI6MTY1Mjc4MTYzM30.NpfugyhGnauxPcW9rdHgj4oEf_GRS84HznepD3X14Sw");
   });
 
   test("upload mainThumbnail", () async {
     final images = await getImage();
-    final mainThumbnail = {
-      "mainThumbnail": "duong link dan toi Thumbnail"
-    };
+    final mainThumbnail = {"mainThumbnail": "duong link dan toi Thumbnail"};
     PostServices services = PostServices();
-    await services.uploadMainThumbnail(id:"625be882ba0392826c179527", mainThumbnail: mainThumbnail  , token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNWJkMDY0OGUxODE0NWE4NTIxMTk0NSIsImlzQWRtaW4iOmZhbHNlLCJzZXNzaW9uVG9rZW4iOiI2MjViZTU0MWQ3Y2UxY2JjMGJiZTZjZjUiLCJpYXQiOjE2NTAxODk2MzMsImV4cCI6MTY1Mjc4MTYzM30.NpfugyhGnauxPcW9rdHgj4oEf_GRS84HznepD3X14Sw" );
+    await services.uploadMainThumbnail(
+        id: "625be882ba0392826c179527",
+        mainThumbnail: mainThumbnail,
+        token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNWJkMDY0OGUxODE0NWE4NTIxMTk0NSIsImlzQWRtaW4iOmZhbHNlLCJzZXNzaW9uVG9rZW4iOiI2MjViZTU0MWQ3Y2UxY2JjMGJiZTZjZjUiLCJpYXQiOjE2NTAxODk2MzMsImV4cCI6MTY1Mjc4MTYzM30.NpfugyhGnauxPcW9rdHgj4oEf_GRS84HznepD3X14Sw");
   });
 
-  test("getPostWithFilter", () async{
+  test("getPostWithFilter", () async {
     Map<String, dynamic> params = {
-      "page":2,
-      "limit":1,
-      "creatorId[eq]":"625bd0648e18145a85211945"
+      "page": 2,
+      "limit": 1,
+      "creatorId[eq]": "625bd0648e18145a85211945"
     };
     PostServices services = PostServices();
     await services.getPostWithFilter(params: params);
   });
   test("getPost", () async {
     PostServices services = PostServices();
-    await services.getPostDetail(id:"625bd215fa9050c1dc04fdb4");
+    await services.getPostDetail(id: "625bd215fa9050c1dc04fdb4");
   });
 
   test("updateAllPost", () async {
-    Map<String,dynamic> update = {
-      "desc":"123"
-    };
+    Map<String, dynamic> update = {"desc": "123"};
     PostServices services = PostServices();
-    await services.updateAllPost(updateInfo: update, token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNmE2ZWIzODAzYjFlMjIxZGQyYzJjYiIsImlzQWRtaW4iOnRydWUsInNlc3Npb25Ub2tlbiI6IjYyNmE2ZWJiODAzYjFlMjIxZGQyYzJjZSIsImlhdCI6MTY1MTE0MjMzMSwiZXhwIjoxNjUzNzM0MzMxfQ.BkGKAAAa6sFJPQ1vTn9zQg59E0Cd7eRznFECbgeCn5k");
+    await services.updateAllPost(
+        updateInfo: update,
+        token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNmE2ZWIzODAzYjFlMjIxZGQyYzJjYiIsImlzQWRtaW4iOnRydWUsInNlc3Npb25Ub2tlbiI6IjYyNmE2ZWJiODAzYjFlMjIxZGQyYzJjZSIsImlhdCI6MTY1MTE0MjMzMSwiZXhwIjoxNjUzNzM0MzMxfQ.BkGKAAAa6sFJPQ1vTn9zQg59E0Cd7eRznFECbgeCn5k");
   });
 
   test("updatePost", () async {
-    Map<String,dynamic> update = {
-      "desc":"123"
-    };
+    Map<String, dynamic> update = {"desc": "123"};
     PostServices services = PostServices();
-    await services.updatePost(id:"625a6ae0c313e60d2f3836e6" ,updateInfo: update, token: "example1");
+    await services.updatePost(
+        id: "625a6ae0c313e60d2f3836e6", updateInfo: update, token: "example1");
   });
-  
+
   test("removeMask", () async {
     File image = File("assets/test/mask1.jpg");
     Uint8List data = await image.readAsBytes();
@@ -124,8 +137,23 @@ void main() {
     Uint8List data2 = await image2.readAsBytes();
     String base64Mask = base64Encode(data2);
     InPaintingServies services = InPaintingServies();
-    await services.removeWithMask(base64Image: base64Image, base64Mask: base64Mask);
-    
+    await services.removeWithMask(
+        base64Image: base64Image, base64Mask: base64Mask);
   });
 
+  test("login", () async {
+    const account = {"username": "mai", "password": "example1"};
+    AuthServices authServices = AuthServices();
+    await authServices.login(account: account);
+  });
+
+  test("signup", () async {
+    const account = {
+      "username": "minh23",
+      "email": "minh23@gmail.com",
+      "password": "example123"
+    };
+    AuthServices authServices = AuthServices();
+    await authServices.signup(account: account);
+  });
 }

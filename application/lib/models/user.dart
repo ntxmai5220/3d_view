2681b042df065e1596c5d5dccd902d81824427f3
ntxmai5220 <xuanmai.k18@gmail.com>
2022-05-07 12:37:37 +0700

    update user model

--- application/lib/models/user.dart
+++ application/lib/models/user.dart
@@ -1,19 +1,13 @@
-import 'package:json_annotation/json_annotation.dart';
-
-
-part 'user.g.dart';
-
-@JsonSerializable()
-class User{
+class User {
   User({
     required this.id,
     required this.username,
     required this.email,
     required this.isAdmin,
     required this.posts,
-    required this.accessToken
+    required this.accessToken,
   });
-  @JsonKey(name: '_id')
+
   String id;
   String username;
   String email;
@@ -21,9 +15,25 @@ class User{
   List<String> posts;
   String accessToken;
 
-  factory User.fromJson(Map<String,dynamic> json) => _$UserFromJson(json);
-  
-  Map<String, dynamic> toJson() => _$UserToJson(this);
+  factory User.fromJson(Map<String, dynamic> map) {
+    return User(
+      id: map['id'] ?? '',
+      username: map['username'] ?? '',
+      email: map['email'] ?? '',
+      isAdmin: map['isAdmin'] ?? false,
+      posts: List<String>.from(map['posts']),
+      accessToken: map['accessToken'] ?? '',
+    );
+  }
 
-
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'username': username,
+      'email': email,
+      'isAdmin': isAdmin,
+      'posts': posts,
+      'accessToken': accessToken,
+    };
+  }
 }
